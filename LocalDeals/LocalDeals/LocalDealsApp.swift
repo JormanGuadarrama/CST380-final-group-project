1eb020e04d382a75d5efa5b44ea5c762b29abd63
Kevin <kcrapo@csumb.edu>
2026-04-12 12:19:23 -0700

    Init Commit
    Swift ui app default project

---
+++ LocalDeals/LocalDeals/LocalDealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalDealsApp.swift
+//  LocalDeals
+//
+//  Created by Kevin Crapo on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct LocalDealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
